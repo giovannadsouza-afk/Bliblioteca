3303574d6096ff8d297db97332af37fb9c18f60e
GIOVANNA DIAS PIRES DE SOUZA <giovanna.d.souza@edu.senai.br>
2026-04-28 09:10:28 -0300

    Update entrada.php

--- entrada.php
+++ entrada.php
@@ -10,5 +10,10 @@ use  function conversao\libraParaReal;
 use  function conversao\pesoMexicanoParaReal;
 use  function conversao\ieniParaReal;
 
+echo "A conversão deu " , dolarParaReal (100 , 5)
+echo "A conversão deu " , euroParaReal (100 , 5,85)
+echo "A conversão deu " , libraParaReal (100 , 6,72)
+echo "A conversão deu " , pesoMexicano (100, 0,29 )
+echo "A conversão deu " , ieniParaReal (100, 0,031)
 
 ?>;
